dd1e9833f4d9816e3322ecfb6aed9ab9603f7bc8
slarge <slarge@88ac59c5-452e-42d9-a310-fa019040a976>
2015-06-14 22:42:06 +0000

    Add SVN keywords to provide revision/date stamping for files.

--- trunk/src/printf.c
+++ trunk/src/printf.c
@@ -3,6 +3,9 @@ es-printf  -  configurable printf for embedded systems
 
 printf.c: Main source file for printf family of functions.
 
+$Revision$
+$Date$
+
 **************************************************************************
 Copyright (c) 2006 - 2013 Skirrid Systems
 
